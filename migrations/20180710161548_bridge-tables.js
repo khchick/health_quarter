26c50a1abb5246275a6370c6c5ba57c541465aeb
Sonya Wong <sonyawongpro@gmail.com>
2018-07-17 14:08:01 +0800

    push

--- migrations/20180710161548_bridge-tables.js
+++ migrations/20180710161548_bridge-tables.js
@@ -10,6 +10,7 @@ exports.up = function (knex) {
             //FK: tag.id 
             table.integer('tag_id').unsigned();
             table.foreign('tag_id').references('tag.id');
+            table.timestamps(false, true);
         }),
 
         knex.schema.createTable('users_fav_restaurant', function (table) {
@@ -22,6 +23,7 @@ exports.up = function (knex) {
             //FK: users.id 
             table.integer('users_id').unsigned();
             table.foreign('users_id').references('users.id');
+            table.timestamps(false, true);
         }),
 
         knex.schema.createTable('users_fav_meal_plan', function (table) {
@@ -34,6 +36,7 @@ exports.up = function (knex) {
             //FK: meal_plan.id
             table.integer('meal_plan_id').unsigned();
             table.foreign('meal_plan_id').references('meal_plan.id');
+            table.timestamps(false, true);
         }),
 
         knex.schema.createTable('users_fav_recipe', function (table) {
@@ -43,6 +46,7 @@ exports.up = function (knex) {
             //FK: users.id
             table.integer('users_id').unsigned();
             table.foreign('users_id').references('users.id');
+            table.timestamps(false, true);
         }),
 
         knex.schema.createTable('users_fav_dish', function (table) {
@@ -55,6 +59,7 @@ exports.up = function (knex) {
             //FK: dish.id 
             table.integer('dish_id').unsigned();
             table.foreign('dish_id').references('dish.id');
+            table.timestamps(false, true);
         }),
 
         knex.schema.createTable('users_fav_tag', function (table) {
@@ -67,6 +72,7 @@ exports.up = function (knex) {
             //FK: dish.id 
             table.integer('tag_id').unsigned();
             table.foreign('tag_id').references('tag.id');
+            table.timestamps(false, true);
         }),
     ]);
 }
