4c8c852ca8c80f45c8513d540a547d7b99b64d00
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2018-07-18 17:23:35 +0800

    working buttons and database insertion

--- public/js/favRec.js
+++ public/js/favRec.js
@@ -7,7 +7,7 @@ $(() => {
     const Recipe = (recURL) => {
         return `
                 <div class="info-container">
-                    <label class="lbl-info">Full URL: </label><p><strong>${recURL}</strong></p>
+                    <label class="lbl-info">Name and ID: </label><p><strong>${recURL}</strong></p>
                     <label class="lbl-info">Full Information: </label><p><a href='https://spoonacular.com/${recURL}'>View details</a></p>
                 </div>`
     };
