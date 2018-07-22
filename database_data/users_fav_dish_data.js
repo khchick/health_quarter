87d5913a32a460345b72f4d142dc4b24e3ccae0a
Sonya Wong <sonyawongpro@gmail.com>
2018-07-22 22:53:43 +0800

    expanded database_data

--- database_data/users_fav_dish_data.js
+++ database_data/users_fav_dish_data.js
@@ -1,8 +1,10 @@
 module.exports = [
     {users_id: 1, dish_id: 1},
     {users_id: 1, dish_id: 2},
+    {users_id: 1, dish_id: 12},
     {users_id: 2, dish_id: 1},
     {users_id: 2, dish_id: 2},
+    {users_id: 2, dish_id: 10},
     {users_id: 2, dish_id: 1},
     {users_id: 3, dish_id: 2},
     {users_id: 3, dish_id: 3},
