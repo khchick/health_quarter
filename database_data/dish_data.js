ddcddb6902b7f4afa5103dd2bba09ca503955936
Sonya Wong <sonyawongpro@gmail.com>
2018-07-23 18:13:07 +0800

    updated all restaurants images

--- database_data/dish_data.js
+++ database_data/dish_data.js
@@ -1,47 +1,27 @@
 module.exports = [
     {
-        name: 'Carrot and Beets Soup',
-        img: '/images/carrot-and-beets-soup.jpg',
-        rest_id: 1
-    },
-    {
         name: 'Confusion Beyond Burger',
-        img: '/images/Confusion-Beyond-Burger.jpg',
+        img: '/images/confusion/Confusion-Beyond-Burger.jpg',
         rest_id: 1
     },
     {
         name: 'Confusion Indian Platter',
-        img: '/images/Confusion-Indian-Platter.jpg',
+        img: '/images/confusion/Confusion-Indian-Platter.jpg',
         rest_id: 1
     },
     {
         name: 'Confusion Vegan Wrap',
-        img: '/images/Confusion-Vegan-Wrap.jpg',
-        rest_id: 1
-    },
-    {
-        name: 'Karage Hedgehog Mushrooms',
-        img: '/images/karage-hedgehog-mushrooms.jpg',
-        rest_id: 1
-    },
-    {
-        name: 'Spaghetti Meat Meatballs',
-        img: '/images/spaghetti-meat-meatballs.jpg',
-        rest_id: 1
-    },
-    {
-        name: 'Vegan Food Grad With Sourdough Toast',
-        img: '/images/vegan-food-grad-with-sourdough-toast.jpg',
+        img: '/images/confusion/Confusion-Vegan-Wrap.jpg',
         rest_id: 1
     },
     {
         name: 'Smoothie Bowls',
-        img: '/images/The_Cakery_Smoothie_Bowls.jpg',
+        img: '/images/cakery/The_Cakery_Smoothie_Bowls.jpg',
         rest_id: 2
     },
     {
         name: 'Vegan Latte',
-        img: '/images/The_Cakery_vegan_latte.jpg',
+        img: '/images/cakery/The_Cakery_vegan_latte.jpg',
         rest_id: 2
     },
     {
